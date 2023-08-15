fc401d7e334052f809ac35d4580049637574e950
manojts <manojmanuts81@gmail.com>
2023-08-15 16:17:02 -0400

    Searching in nearly sorted array

--- MainBinarySearch.java
+++ MainBinarySearch.java
@@ -13,6 +13,11 @@ public class MainBinarySearch {
 		element = 2;
 		binarySearchCountFrequency(array1, element);
 		
+		int array2[]= {6,7,8,9,10,1,2,3,4,5};
+		System.out.println("Number of times array is rotated " + numberOfTimesArrayIsRotated(array2));
+		element = 9;
+		System.out.println("Element "+ element + " is found at " + searchingInNearlySortedRotated(array2, element));
+
 
 	}
 	public static int binarySearch(int array[], int element) {
@@ -72,9 +77,51 @@ public class MainBinarySearch {
 		System.out.println("First occurence of the element is at " + first);
 		System.out.println("last occurence of the element is at " + last);
 		System.out.println("Count of the element is " + frequency);
+	}
+	public static int numberOfTimesArrayIsRotated(int array[]) {
+		int start = 0;
+		int end = array.length - 1;
+		int mid = (start + end)/2;
+		while(start<=end) {
+			mid = (start + end)/2;
+			int prev = (mid + array.length - 1)% array.length;
+			int next = (mid + 1)%array.length;
+			if(array[prev] <= array[mid] && array[mid] <= array[next]) {
+				return mid;
+			}
+			else if(array[mid] < array[end]) {
+				end = mid - 1;
+			}
+			else {
+				start = mid + 1;
+			}
+		}
+		return -1;
+	}
 
-
-		
+	public static int searchingInNearlySortedRotated(int array[], int element) {
+		int start = 0;
+		int end = array.length - 1;
+		int mid = (start + end)/2;
+		while(start<=end) {
+			mid = (start + end)/2;
+			if(array[mid] == element) {
+				return mid;
+			}
+			if(start <= mid - 1 && array[mid-1] == element) {
+				return mid-1;
+			}
+			if(end<= mid +1 && array[mid+1] == element) {
+				return mid+1;
+			}
+			else if(array[mid] < element) {
+				end = mid - 2;
+			}
+			else {
+				start = mid + 2;
+			}
+		}
+		return -1;
 	}
 
 }
